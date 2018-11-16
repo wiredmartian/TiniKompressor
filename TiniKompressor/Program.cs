eb7201977a26adbc19c16a3987f0dc0e5d6a7c77
Solomzi Jikani <solomzi.jikani@nologostudios.com>
2018-11-16 14:29:28 +0200

    Kompressed

--- TiniKompressor/Program.cs
+++ TiniKompressor/Program.cs
@@ -2,6 +2,8 @@ using System;
 using System.Threading.Tasks;
 using TinifyAPI;
 using System.IO;
+using System.Collections.Generic;
+using System.Configuration;
 
 namespace TiniKompressor
 {
@@ -9,30 +11,27 @@ namespace TiniKompressor
     {
         static void Main(string[] args)
         {
-            Program p = new Program();
             Console.WriteLine("Paste path directory with images");
             var dir = Console.ReadLine();
-            p.GetDirectoriesInAFolder(dir);
-            //int x = p.GetFilesFromDir(dir);
-            //MainAsync(args).GetAwaiter().GetResult();
+            MainAsync(args, dir).GetAwaiter().GetResult();
         }
-        static async Task MainAsync(string[] args) {
-
-            //Tinify.Key = "cn978rXplwsVpq9Pgtsrddw9JfRHF6z5";
-            //var source = Tinify.FromFile("bugs.jpg");
-            //await source.ToFile("bugs_min.jpg");
-
-            //Console.WriteLine("Done...", source);
-        }
-
-        private void GetDirectoriesInAFolder(string path)
-        {
+        static async Task MainAsync(string[] args, string path) {
+            Program p = new Program();
+            Tinify.Key = ConfigurationManager.AppSettings["TinyKey"];
+            List<FileInfo> files = new List<FileInfo>();
             if (Directory.Exists(path))
             {
-                DirectoryInfo info = new DirectoryInfo(path);
-
-                GetAllDirectories(info);
+                files = p.TraverseTree(path);
+                Console.WriteLine("\n------------------ {0} FILES FOUND!--------------------\n", files.Count);
+                foreach (FileInfo f in files)
+                {
+                    long length = f.Length;
+                    var source = Tinify.FromFile(f.FullName);
+                    await source.ToFile(f.FullName);
+                    Console.WriteLine("{0} compressed successfully!", f.Name, length, f.Length);
+                }
             }
+            Console.WriteLine("\n Complete! {0} files compressed. Press Enter to closed", files.Count);
         }
 
         private void GetAllDirectories(DirectoryInfo directoryInfo)
@@ -40,7 +39,7 @@ namespace TiniKompressor
             FileInfo[] files = null;
             DirectoryInfo[] subDirs = null;
 
-            files = directoryInfo.GetFiles("*.*");
+            files = directoryInfo.GetFiles("*.jpg*");
             if (files != null)
             {
                 foreach (FileInfo f in files)
@@ -54,6 +53,64 @@ namespace TiniKompressor
                     GetAllDirectories(dirInfo);
                 }
             }
+        }
+        public List<FileInfo> TraverseTree(string root)
+        {
+            Stack<string> dirs = new Stack<string>();
+            List<FileInfo> _files = new List<FileInfo>();
+
+            if (!Directory.Exists(root))
+            {
+                throw new ArgumentException();
+            }
+            dirs.Push(root);
+
+            while (dirs.Count > 0)
+            {
+                string currentDir = dirs.Pop();
+                string[] subDirs;
+                try
+                {
+                    subDirs = Directory.GetDirectories(currentDir);
+                }
+                catch (UnauthorizedAccessException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                catch (DirectoryNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                string[] files = null;
+                try
+                {
+                    files = Directory.GetFiles(currentDir);
+                }
+
+                catch (UnauthorizedAccessException e)
+                {
+
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+
+                catch (DirectoryNotFoundException e)
+                {
+                    Console.WriteLine(e.Message);
+                    continue;
+                }
+                foreach (string file in files)
+                {
+                    FileInfo f = new FileInfo(file);
+                    _files.Add(f);
+                }
+                foreach (string str in subDirs)
+                    dirs.Push(str);
+            }
+            return _files;
         }
     }
 }
